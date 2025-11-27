1cfa9212b9ccbaef2f14da0d7f5d69987ec03636
Johan Camilo Calderon Alvarez <jjcalderon.4697@unicesmag.edu.co>
2025-11-26 19:22:27 -0500

    Correccion

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,7 +1,7 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://pnwkthenanilunzkycdi.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBud2t0aGVuYW5pbHVuemt5Y2RpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxODQ3MTQsImV4cCI6MjA3OTc2MDcxNH0.uta68BswK3lLksoDZUVjEboEJ-N4z2Pb-gvkkwMRwT8';
+    const SUPABASE_URL = 'https://tctqlythioyhjkisdhbl.supabase.co';
+    const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjdHFseXRoaW95aGpraXNkaGJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxODc1NzYsImV4cCI6MjA3OTc2MzU3Nn0.zAenvnPc_3jK_pyl-pdhnHiMSo5XhHvGmTElexcBkj4';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY); 
